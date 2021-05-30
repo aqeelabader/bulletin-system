0932deacf1278101439339cb1d57adff56ff5020
Aqeel <abader.aqeel@gmail.com>
2021-05-30 21:23:19 +0200

    id fixed

--- src/app/bulletin.service.ts
+++ src/app/bulletin.service.ts
@@ -1,6 +1,7 @@
 import { Bulletin } from './bulletin.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 
 
@@ -9,16 +10,26 @@ export class BulletinService{
   private bulletins: Bulletin [] = [];
   private updatedBulletins = new Subject<Bulletin[]>();
 
+  constructor(private http: HttpClient){}
   getBulletins(){
-    return [...this.bulletins];
-
+    this.http.get<{message: string, bulletins: Bulletin[]}>('https://localhost:3000/api/bulletins')
+    .subscribe((bulletinsData)=>{
+      this.bulletins = bulletinsData;
+      this.updatedBulletins.next([...this.bulletins]);
+    });
 
   }
 
-  addBulletins(userName: string , emailAddress: string , bulletinDetails: string ){
-    const bulletin: Bulletin = {userName : userName , emailAddress : emailAddress , bulletinDetails : bulletinDetails};
-    this.bulletins.push(bulletin);
-    this.updatedBulletins.next([...this.bulletins]); //from vid
+  addBulletins(userName: string , emailAddress: string , bulletinDetails: string )
+  {
+    const bulletin: Bulletin = {id:'' , userName : userName , emailAddress : emailAddress , bulletinDetails : bulletinDetails};
+    this.http.post<{message: String} >('https://localhost:3000/api/bulletins',bulletin)
+    .subscribe((responseBulletinData)=>{
+      console.log(responseBulletinData.message);
+      this.bulletins.push(bulletin);
+      this.updatedBulletins.next([...this.bulletins]);     //from vid
+
+    });
   }
 
 
